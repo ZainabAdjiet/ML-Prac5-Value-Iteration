47bb7f8151981c182e9df6647320a06848c529ce
Zainab <zainab.adjiet@gmail.com>
2019-09-19 18:03:17 +0200

    setup methods needed and added comparator for transitions

--- src/val_iter.h
+++ src/val_iter.h
@@ -2,16 +2,34 @@
 #define MAIN_H
 
 /****************************************************************/
-/* Includes														*/
+/* Includes
 /****************************************************************/
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 /****************************************************************/
-/* Functions												    */
+/* Functions
 /****************************************************************/
 
-namespace ADJZAI001 {}
+namespace ADJZAI001_val_iter {
+
+    struct transition {
+        int state_num;
+        float reward;
+
+        bool operator<(transition other);
+    };
+
+    // data structures used for value iteration algorithm
+    std::vector< std::vector<transition> > transitions;
+    std::vector<float> V;
+    std::vector<int> PI;
+
+    // value iteration methods
+    float bellman_value(int state);
+    int next_max_state(int state);
+}
 
 #endif
